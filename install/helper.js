72a9d45550e79793e4304443cc3bd994c8c2811c
Zack Bloom <zackbloom@gmail.com>
2015-01-24 03:58:30 -0500

    Update install code

--- install/helper.js
+++ install/helper.js
@@ -2,11 +2,60 @@
   var elements = INSTALL_OPTIONS.elements;
 
   for (var i=0; i < elements.length; i++){
-    var els = document.querySelectorAll(elements[i].selector);
-    if (!els) continue;
+    (function(i){
 
-    for (var j=0; j < els.length; j++){
-      els[j].className += " hvr-" + elements[i].effect;
-    }
+      var els = document.querySelectorAll(elements[i].selector);
+      if (!els) continue;
+
+      var cls = " animated";
+      if (elements[i].loop) {
+        cls += " infinite";
+      }
+      cls += " " + elements[i].animation;
+
+      var trigger = function(j){
+        els[j].className += cls;
+        setTimeout(function(){
+          reset(j);
+        }, 1000);
+      }
+      var reset = function(j){
+        els[j].className = els[j].className.replace(cls, '');
+      }
+
+      switch (elements[i].when){
+      case "page-load":
+        for (var j=0; j < els.length; j++){
+          trigger(j);
+        }
+
+        break;
+      case "hover":
+        for (var j=0; j < els.length; j++){
+          (function(j){
+            els[j].addEventHandler('mouseover', function(){
+              reset(j);
+              trigger(j);
+            });
+            els[j].addEventHandler('mouseout', function(){
+              reset(j);
+            });
+          })(j)
+        }
+
+        break;
+      case "click":
+        for (var j=0; j < els.length; j++){
+          (function(j){
+            els[j].addEventHandler('click', function(){
+              reset(j);
+              trigger(j);
+            });
+          })(j)
+        }
+
+        break;
+      }
+    })(i);
   }
 })()

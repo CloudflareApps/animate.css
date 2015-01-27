b1eee8a7b70cd5c5baafdcbdbc469e3c14134a94
Zack Bloom <zackbloom@gmail.com>
2015-01-27 14:39:07 -0500

    Run on load animations sooner

--- install/helper.js
+++ install/helper.js
@@ -2,66 +2,74 @@
   if (!document.querySelectorAll || !document.addEventListener)
     return;
 
-  var elements = INSTALL_OPTIONS.elements;
+  var load = function(){
+    var elements = INSTALL_OPTIONS.elements;
 
-  for (var i=0; i < elements.length; i++){
-    (function(i){
+    for (var i=0; i < elements.length; i++){
+      (function(i){
 
-      var els = document.querySelectorAll(elements[i].selector);
-      if (!els) return;
+        var els = document.querySelectorAll(elements[i].selector);
+        if (!els) return;
 
-      var cls = " animated";
-      if (elements[i].loop) {
-        cls += " infinite";
-      }
-      cls += " " + elements[i].animation;
-
-      var trigger = function(j){
-        els[j].className += cls;
-
-        if (!elements[i].loop){
-          setTimeout(function(){
-            reset(j);
-          }, 1000);
+        var cls = " animated";
+        if (elements[i].loop) {
+          cls += " infinite";
         }
-      }
-      var reset = function(j){
-        els[j].className = els[j].className.replace(cls, '');
-      }
+        cls += " " + elements[i].animation;
 
-      switch (elements[i].when){
-      case "page-load":
-        for (var j=0; j < els.length; j++){
-          trigger(j);
-        }
+        var trigger = function(j){
+          els[j].className += cls;
 
-        break;
-      case "hover":
-        for (var j=0; j < els.length; j++){
-          (function(j){
-            els[j].addEventListener('mouseover', function(){
+          if (!elements[i].loop){
+            setTimeout(function(){
               reset(j);
-              trigger(j);
-            });
-            els[j].addEventListener('mouseout', function(){
-              reset(j);
-            });
-          })(j)
+            }, 1000);
+          }
+        }
+        var reset = function(j){
+          els[j].className = els[j].className.replace(cls, '');
         }
 
-        break;
-      case "click":
-        for (var j=0; j < els.length; j++){
-          (function(j){
-            els[j].addEventListener('click', function(){
-              reset(j);
-              trigger(j);
-            });
-          })(j)
-        }
+        switch (elements[i].when){
+        case "page-load":
+          for (var j=0; j < els.length; j++){
+            trigger(j);
+          }
 
-        break;
-      }
-    })(i);
+          break;
+        case "hover":
+          for (var j=0; j < els.length; j++){
+            (function(j){
+              els[j].addEventListener('mouseover', function(){
+                reset(j);
+                trigger(j);
+              });
+              els[j].addEventListener('mouseout', function(){
+                reset(j);
+              });
+            })(j)
+          }
+
+          break;
+        case "click":
+          for (var j=0; j < els.length; j++){
+            (function(j){
+              els[j].addEventListener('click', function(){
+                reset(j);
+                trigger(j);
+              });
+            })(j)
+          }
+
+          break;
+        }
+      })(i);
+    }
+  };
+
+  if (document.readyState == 'loading'){
+    document.addEventListener('DOMContentLoaded', load);
+  } else {
+    load();
   }
 })()

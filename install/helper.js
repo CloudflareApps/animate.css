359e33709629691313f105821c52a74ac31fc0e6
Zack Bloom <zackbloom@gmail.com>
2015-10-27 13:12:23 -0400

    Add live updating @adamschwartz

--- install/helper.js
+++ install/helper.js
@@ -2,20 +2,59 @@
   if (!document.querySelectorAll || !document.addEventListener)
     return;
 
+  var options = INSTALL_OPTIONS;
+
+  var boundEvents = [];
+  var addedAnimations = {};
+
   var load = function(){
-    var elements = INSTALL_OPTIONS.elements;
+    var elements = options.elements;
+
+    for (var i=0; i < boundEvents.length; i++){
+      var e = boundEvents[i];
+
+      e[0].removeEventListener(e[1], e[2]);
+    }
+
+    boundEvents = [];
+
+    if (document.body.classList){
+      // Only used by the preview, so it's ok to use classList here
+
+      for (var selector in addedAnimations){
+        var els = document.querySelectorAll(selector);
+        if (!els) continue;
+
+        var prevAdded = addedAnimations[selector];
+
+        for (var j=0; j < els.length; j++){
+          els[j].classList.remove('animated');
+          els[j].classList.remove('infinite');
+
+          for (var k=0; k < prevAdded.length; k++){
+            els[j].classList.remove(prevAdded[k]);
+          }
+        }
+      }
+    }
+
+    addedAnimations = {};
 
     for (var i=0; i < elements.length; i++){
       (function(i){
 
+
         var els = document.querySelectorAll(elements[i].selector);
         if (!els) return;
+
+        addedAnimations[elements[i].selector] = [];
 
         var cls = " animated";
         if (elements[i].loop) {
           cls += " infinite";
         }
         cls += " " + elements[i].animation;
+        addedAnimations[elements[i].selector].push(elements[i].animation);
 
         var trigger = function(j){
           els[j].className += cls;
@@ -30,6 +69,8 @@
           els[j].className = els[j].className.replace(cls, '');
         }
 
+        var handler;
+
         switch (elements[i].when){
         case "page-load":
           for (var j=0; j < els.length; j++){
@@ -40,13 +81,18 @@
         case "hover":
           for (var j=0; j < els.length; j++){
             (function(j){
-              els[j].addEventListener('mouseover', function(){
+              els[j].addEventListener('mouseover', handler = function(){
                 reset(j);
                 trigger(j);
               });
-              els[j].addEventListener('mouseout', function(){
+
+              boundEvents.push([els[j], 'mouseover', handler]);
+
+              els[j].addEventListener('mouseout', handler = function(){
                 reset(j);
               });
+
+              boundEvents.push([els[j], 'mouseout', handler]);
             })(j)
           }
 
@@ -54,10 +100,12 @@
         case "click":
           for (var j=0; j < els.length; j++){
             (function(j){
-              els[j].addEventListener('click', function(){
+              els[j].addEventListener('click', handler = function(){
                 reset(j);
                 trigger(j);
               });
+
+              boundEvents.push([els[j], 'click', handler]);
             })(j)
           }
 
@@ -67,9 +115,19 @@
     }
   };
 
+  var setOptions = function(opts) {
+    options = opts;
+
+    load();
+  };
+
   if (document.readyState == 'loading'){
     document.addEventListener('DOMContentLoaded', load);
   } else {
     load();
   }
+
+  window.EagerAnimate = {
+    setOptions: setOptions
+  };
 })()
